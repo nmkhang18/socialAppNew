4163426f4e00d39c8553da5be5be00f93ff4074b
Minh Khang <minhkhangg18@gmail.com>
2023-11-10 16:35:41 +0700

    update

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -9,7 +9,7 @@ const router = express.Router()
 
 //GET
 router.get('/comment/:post_id', authencation.login, postController.getComment)
-router.get('/user/:user_id', postController.getPostByUser)
+router.get('/user/:user_id', authencation.login, postController.getPostByUser)
 router.get('/detail/:post_id', authencation.login, postController.getPostDetail)
 
 
